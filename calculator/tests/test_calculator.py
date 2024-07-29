43b69fc327232657e864f42e49016e6d32e4f44a
Dhanunjaya-Elluri <dhanunjayet@gmail.com>
2024-07-29 16:21:39 +0200

    Test with pull true from ECR and cache

--- calculator/tests/test_calculator.py
+++ calculator/tests/test_calculator.py
@@ -28,6 +28,7 @@ def test_square():
     assert square(2) == 4
     assert square(3) == 9
     assert square(4) == 16
+    assert square(5) == 25
 
 def test_env():
     assert os.getenv('ENV1') == 'test1'
